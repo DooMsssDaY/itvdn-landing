a122ae9bd3f297468cf7e8fb66e74a403acc9b92
Nick <DooMsssDaY@mail.ru>
2018-05-13 16:02:04 +1000

    validation was added

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,23 @@
 (function() {
 	var openFormButton = document.querySelector('.arrow-down');
+	var form           = document.querySelector('.form');
 
 	if (null !== openFormButton) {
 		openFormButton.addEventListener('click', function() {
-			form.open();
+			ITVDN.form.open();
 		})
 	}
-})();
+
+	if (null !== form) {
+		form.addEventListener('submit', function(e) {
+			e.preventDefault();
+
+			if (true === ITVDN.form.isValid()) {
+				console.log('All is well');
+			}
+			else {
+				console.log('All is not well');
+			}
+		});
+	}
+}());
